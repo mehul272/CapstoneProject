9f80fdbf3680394f93b475bac493c57e1268b9bc
Mehul2718 <110376344+Mehul2718@users.noreply.github.com>
2023-03-25 13:39:25 +0530

    Initial Commit

---
+++ django_ractjs/core/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from .views import FilesViewSet
+
+
+router = DefaultRouter()
+router.register('files',FilesViewSet,basename="files")
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
